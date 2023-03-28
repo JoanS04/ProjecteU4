c56b4eebbf2c3d8cc0427dede1ff90bc0e580e20
JoanS04 <joansanchez@paucasesnovescifp.cat>
2023-03-28 12:05:17 +0200

    javadoc

--- Projecte/src/registro/RegistroUsuario.java
+++ Projecte/src/registro/RegistroUsuario.java
@@ -2,16 +2,30 @@ package registro;
 
 import java.util.Scanner;
 
+
+/**
+ * La clase RegistroUsuario permite a un usuario introducir sus datos de registro y validarlos mediante
+ * la clase ValidarCampo. Al finalizar la introducción de datos, muestra los datos introducidos por el usuario.
+ */
 public class RegistroUsuario {
+
+    String[] usuarios = {"Carles_345", "Sara-238", "Juan_912", "Ana-605", "Luisa_123", "Pablo-896", "Elena_321", "Pedro-789", "Maria_456", "Marta-234"}; // base de datos de los usuarios
     
-    ValidarCampo v = new ValidarCampo();
+    private ValidarCampo v = new ValidarCampo(); // instancia de la clase ValidarCampo para validar los campos
 
-    String[] usuarios = {"Carles_345", "Sara-238", "Juan_912", "Ana-605", "Luisa_123", "Pablo-896", "Elena_321", "Pedro-789", "Maria_456", "Marta-234"};
-
+    /**
+     * Constructor de la clase RegistroUsuario que llama al método init para iniciar el registro de usuario.
+     */
     public RegistroUsuario(){
         init();
     }
 
+    /**
+     * Método privado init que inicia el proceso de registro de usuario.
+     * Solicita al usuario que introduzca su nombre de usuario, correo electrónico y contraseña,
+     * y valida cada uno de los campos mediante la instancia de la clase ValidarCampo.
+     * Una vez validados los campos, muestra los datos introducidos por el usuario.
+     */
     private void init() {
         try (Scanner scanner = new Scanner(System.in)) {
             boolean valid = false;
@@ -31,6 +45,16 @@ public class RegistroUsuario {
         }
     }
 
+    /**
+     * Método privado getStringFromScanner que recibe un objeto Scanner, un mensaje para mostrar al usuario
+     * y un número de comprovación. Según el número de comprovación, llama a un método de la clase ValidarCampo
+     * para validar el campo introducido por el usuario. Retorna el valor introducido por el usuario una vez validado.
+     * @param scanner objeto Scanner utilizado para recibir la entrada del usuario.
+     * @param message mensaje que se muestra al usuario para solicitar el campo.
+     * @param comprovacio número que indica qué campo se está validando (1=nombre de usuario, 2=correo electrónico,
+     *                    3=contraseña, 4=código de verificación).
+     * @return el valor introducido por el usuario una vez validado.
+     */
     private String getStringFromScanner(Scanner scanner, String message, int comprovacio) {
         boolean acceptat = false;
         String valor = "";
@@ -49,10 +73,12 @@ public class RegistroUsuario {
 
         return valor;
     }
-    
+
+    /**
+     * Método main que crea una instancia de la clase RegistroUsuario.
+     */
     public static void main(String[] args) {
         new RegistroUsuario();
     }
 
 }
-
--- Projecte/src/registro/ValidarCampo.java
+++ Projecte/src/registro/ValidarCampo.java
@@ -5,12 +5,31 @@ import java.util.Arrays;
 import java.util.Scanner;
 import java.util.regex.*;
 
+/**
+* Clase ValidarCampo que contiene métodos para validar campos de entrada en un formulario de registro.
+*/
+
 public class ValidarCampo 
 {
+    /**
+     *Constante que contiene los caracteres permitidos para generar un código aleatorio.
+     */
     private final String CARACTERES = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()_+-=[]{}|;:,.<>/?";
+
+    /**
+     *Variable que almacena el código aleatorio generado.
+     */
     String codigo = "";
+
+    /**
+     *Objeto Scanner para leer datos de entrada del usuario.
+     */
     Scanner sc = new Scanner(System.in);
 
+    /**
+     * Genera un código aleatorio de la longitud especificada y lo muestra por pantalla.
+     * @param longitud Longitud del código aleatorio a generar.
+     */
     private void generarCodigoAleatorio(int longitud) {
         SecureRandom random = new SecureRandom();
         for (int i = 0; i < longitud; i++) {
@@ -20,6 +39,10 @@ public class ValidarCampo
         System.out.println(codigo);
     }
 
+    /**
+     * Verifica si el código ingresado por el usuario es igual al código generado aleatoriamente.
+     * @return true si el código ingresado es igual al código generado, false en caso contrario.
+     */
     public Boolean verificarCodigo() 
     {
         generarCodigoAleatorio(8);
@@ -36,8 +59,11 @@ public class ValidarCampo
         }
     }
 
-    
-    
+    /**
+     * Comprueba si la contraseña cumple con un patrón específico.
+     * @param password Contraseña a comprobar.
+     * @return true si la contraseña cumple con el patrón, false en caso contrario.
+    */
     public boolean compruebaPassword(String password) {
         String patron = "^[A-Z][\\w]*[@#\\-_]\\d{2}$";
         
@@ -52,9 +78,16 @@ public class ValidarCampo
 
     }
     
-    public boolean compruebaNombre(String nombreUsuario, String[] usuarios) {
-        boolean nombreValido = nombreUsuario.matches("^[A-Z][a-z]+[_-]\\d{3}$") && nombreUsuario.length() <= 16;
-        boolean nombreExiste = Arrays.stream(usuarios).anyMatch(n -> n != null && n.equalsIgnoreCase(nombreUsuario));
+    
+    /**
+     * Comprueba si el nombre de usuario cumple con un patrón específico y si no existe ya en el sistema.
+     * @param nombreDeUsuario Nombre de usuario a comprobar.
+     * @param usuarios Lista de usuarios registrados en el sistema.
+     * @return true si el nombre de usuario cumple con el patrón y no existe en el sistema, false en caso contrario.
+     */
+    public boolean compruebaNombre(String nombreDeUsuario, String[] usuarios) {
+        boolean nombreValido = nombreDeUsuario.matches("^[A-Z][a-z]+[_-]\\d{3}$") && nombreDeUsuario.length() <= 16;
+        boolean nombreExiste = Arrays.stream(usuarios).anyMatch(n -> n != null && n.equalsIgnoreCase(nombreDeUsuario));
 
         if (nombreValido && !nombreExiste){
             return true;
@@ -62,6 +95,11 @@ public class ValidarCampo
         return false;
     }
     
+    /**
+     * Comprueba si el correo electrónico cumple con un patrón específico y si el dominio es uno de los aceptados.
+     * @param email Correo electrónico a comprobar.
+     * @return true si el correo electrónico cumple con el patrón y el dominio es uno de los aceptados, false en caso contrario.
+     */
     public boolean compruebaEmail(String email) {
         String[] dominiosAceptados = {"paucasesnovescifp", "yahoo", "gmail", "hotmail"};
         String regexEmail = ".+@.+.(com|es|cat)$";
